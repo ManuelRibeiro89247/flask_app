b79829643324b23228c2704c057e37e96b44b030
ManuelRibeiro89247 <manuelmribeiro2015@gmail.com>
2023-07-13 22:54:47 +0100

    teste 2

--- app/__init__.py
+++ app/__init__.py
@@ -4,46 +4,54 @@ from tensorflow import keras
 import json
 import os
 
+
+from flask_restful import Api
+from .api.handler import HandlerRes
+
 app = Flask(__name__)
+api = Api(app, prefix="/api")
+
+
+api.add_resource(HandlerRes, "/profiles")
 #run_with_ngrok(app)
 
 
-current_directory = os.path.dirname(os.path.abspath(__file__))
+#current_directory = os.path.dirname(os.path.abspath(__file__))
 
 
-model_json_path = os.path.join(current_directory, 'files', 'model.json')
-weights_path = os.path.join(current_directory, 'files', 'weights.h5')
-config_json_path = os.path.join(current_directory, 'files', 'config.json')
+#model_json_path = os.path.join(current_directory, 'files', 'model.json')
+#weights_path = os.path.join(current_directory, 'files', 'weights.h5')
+#config_json_path = os.path.join(current_directory, 'files', 'config.json')
 
 
-with open(model_json_path, 'r') as f:
-    model_json = json.load(f)
+#with open(model_json_path, 'r') as f:
+#    model_json = json.load(f)
 
-model = keras.models.model_from_json(json.dumps(model_json))
-
-
-model.load_weights(weights_path)
+#model = keras.models.model_from_json(json.dumps(model_json))
 
 
-with open(config_json_path, 'r') as f:
-    classes = json.load(f)
+#model.load_weights(weights_path)
+
+
+#with open(config_json_path, 'r') as f:
+#    classes = json.load(f)
 
 @app.route("/")
 def home():
     return "<h1>Welcome</h1>"
 
-@app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    data = request.get_json()
-    input_data = data['inputs']
-    predictions = model.predict(input_data)
-    results = []
-    for prediction in predictions:
-        class_index = prediction.argmax()
-        predicted_class = classes[class_index]
-        result = {'class': predicted_class, 'confidence': float(prediction[class_index])}
-        results.append(result)
-    return jsonify(results)
+#@app.route('/predict', methods=['GET', 'POST'])
+#def predict():
+#    data = request.get_json()
+#    input_data = data['inputs']
+#    predictions = model.predict(input_data)
+#    results = []
+#    for prediction in predictions:
+#        class_index = prediction.argmax()
+#        predicted_class = classes[class_index]
+#        result = {'class': predicted_class, 'confidence': float(prediction[class_index])}
+#        results.append(result)
+#    return jsonify(results)
 
 #app.run()
 
